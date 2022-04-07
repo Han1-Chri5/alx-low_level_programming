234864b82afaffabddc70c980d7bd532b1a66d24
Han1-Chri5 <hanichris16@gmail.com>
2022-04-07 16:26:33 -0700

    Removed trailing whitespace in 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingial puzzle
+ * main - prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
