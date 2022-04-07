6f1cfca7af873a8767d3e9897a8e2c2ac76e4825
Han1-Chri5 <hanichris16@gmail.com>
2022-04-07 13:54:25 -0700

    Corrections to main.c code

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -9,3 +9,18 @@ int main (void)
 {
 	return (0);
 }
+root@eee361b92daf:~/c/0x00$ export CFILE-main.c
+root@eee361b92daf:~/c/0x00$ ./0-preprocessor
+root@eee361b92daf:~/c/0x00$ tail c
+# 942 "/usr/include/stdio.h" 3 4
+
+
+# 2 "main c" 2
+
+
+
+# 3 "main.c"
+int main (void)
+{
+ return (0);
+} 
