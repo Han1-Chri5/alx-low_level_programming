bb161f7b922674cccfc7554e295435768815e2b4
Han1-Chri5 <hanichris16@gmail.com>
2022-04-07 14:08:09 -0700

    More corrections to this C file

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void) 
 {
 	return (0);
 }
@@ -14,13 +14,11 @@ root@eee361b92daf:~/c/0x00$ ./0-preprocessor
 root@eee361b92daf:~/c/0x00$ tail c
 # 942 "/usr/include/stdio.h" 3 4
 
-
 # 2 "main c" 2
 
 
-
 # 3 "main.c"
-int main (void)
+int main(void) 
 {
  return (0);
 } 
