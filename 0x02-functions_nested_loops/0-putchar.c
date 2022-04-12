31ff811d7c5ad70b935455a31e771ba839926c7a
Han1-Chri5 <hanichris16@gmail.com>
2022-04-12 08:14:16 -0700

    Corrections to task 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "_putchar.c"
+#include "main.h"
+
 /**
- * main - main block
- * Prints _putchar followed by a new line
- * Does not include standard libraries.
- * Return: 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char putchar[] = "_putchar";
-	int i = 0;
-
-	while (putchar[i] != '\0')
-	{
-		_putchar(putchar[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
