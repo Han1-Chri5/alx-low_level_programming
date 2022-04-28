28752c95b1d57f61cb242595f00d04908285a207
Han1-Chri5 <hanichris16@gmail.com>
2022-04-28 09:29:01 -0700

    Fixed 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,15 @@
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: string
- *
- * Return: Success 1
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
-	{
+	if (*s == '\0')
 		_putchar('\n');
-		return;
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
